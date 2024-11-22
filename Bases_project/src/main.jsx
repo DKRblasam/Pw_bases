3df0e9c1bde9a0b3ba39ef729098918d6b841efb
Alondev <aloncnafarratejos13@gmail.com>
2024-11-22 08:51:55 -0600

    update

--- Bases_project/src/main.jsx
+++ Bases_project/src/main.jsx
@@ -12,9 +12,35 @@ content.render(
   format={'G00 X+5.3 Y+5.3 Z+5.3'} img={'/public/G00.webp'} 
   notes={'Al encendido, después de M02, M30, EMERGENCIA o RESET, el control asume el código G00. El código G00 es modal e incompatible con G01, G02, G03, G33. <br> Al programar la función G00, no se anula la última F programada, es decir, cuando se programa de nuevo G01, G02 o G03 se recupera dicha F.'}/>
 
-  <Card title={'G01 Interpolacion Lineal'} 
+  <Card title={'G01 Interpolacion circular a derechas'} 
+  descrip={'Los movimientos programados a continuacion de G02 se ejecutan en forma de trayectoria circular al avance de F programado. La interpolacion circular solo se puede ejecutar en el plano'} 
+  format={'G01 X+/-5.3 Y+/-5.3 Z+/-5.3'} img={'/public/G00.webp'} 
+  notes={'El codigo G01 es modal e incompatible con G00,G02,G03,G33.'}/>
+
+<Card title={'G02 Interpolacion Lineal'} 
   descrip={'Los desplazamientos programados despues de G01  se Ejecutan segun una linea recta al avance F programado. Si no se programa el codigo F, el avance sera 0.'} 
   format={'G01 X+/-5.3 Y+/-5.3 Z+/-5.3'} img={'/public/G00.webp'} 
   notes={'El codigo G01 es modal e incompatible con G00,G02,G03,G33.'}/>
+
+<Card title={'G01 Interpolacion Lineal'} 
+  descrip={'Los desplazamientos programados despues de G01  se Ejecutan segun una linea recta al avance F programado. Si no se programa el codigo F, el avance sera 0.'} 
+  format={'G01 X+/-5.3 Y+/-5.3 Z+/-5.3'} img={'/public/G00.webp'} 
+  notes={'El codigo G01 es modal e incompatible con G00,G02,G03,G33.'}/>
+
+<Card title={'G01 Interpolacion Lineal'} 
+  descrip={'Los desplazamientos programados despues de G01  se Ejecutan segun una linea recta al avance F programado. Si no se programa el codigo F, el avance sera 0.'} 
+  format={'G01 X+/-5.3 Y+/-5.3 Z+/-5.3'} img={'/public/G00.webp'} 
+  notes={'El codigo G01 es modal e incompatible con G00,G02,G03,G33.'}/>
+
+<Card title={'G01 Interpolacion Lineal'} 
+  descrip={'Los desplazamientos programados despues de G01  se Ejecutan segun una linea recta al avance F programado. Si no se programa el codigo F, el avance sera 0.'} 
+  format={'G01 X+/-5.3 Y+/-5.3 Z+/-5.3'} img={'/public/G00.webp'} 
+  notes={'El codigo G01 es modal e incompatible con G00,G02,G03,G33.'}/>
+
+<Card title={'G01 Interpolacion Lineal'} 
+  descrip={'Los desplazamientos programados despues de G01  se Ejecutan segun una linea recta al avance F programado. Si no se programa el codigo F, el avance sera 0.'} 
+  format={'G01 X+/-5.3 Y+/-5.3 Z+/-5.3'} img={'/public/G00.webp'} 
+  notes={'El codigo G01 es modal e incompatible con G00,G02,G03,G33.'}/>
+
 </React.Fragment>
 );
