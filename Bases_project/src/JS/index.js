e5fa5b628e3a5c9b244838ac5a83fd359b54059c
JDM_David <fer.mtz.guz@gmail.com>
2024-11-25 19:46:29 -0600

    "Added script tag to multiple HTML files, updated index.js to use ReactDOM.createRoot"

--- Bases_project/src/JS/index.js
+++ Bases_project/src/JS/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css'; // Importa el archivo CSS de Tailwind
 import App from './App';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
